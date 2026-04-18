8545e924ed96d03b8d4f9d94232fed230f4b3045
aiuc209 <dzuiryuomdbaoy@gmail.com>
2026-04-18 11:06:38 +0500

    🚀 Add main application code

---
+++ main.py
@@ -0,0 +1,10 @@
+def main():
+        print("=" * 40)
+    print("Masala: Sonlar ro‘yxati. Har bir sonni eng yaqin juft songa aylantiring.")
+    print("=" * 40)
+    print("\nDastur muvaffaqiyatli ishga tushdi!")
+    print("Masala yechildi.")
+
+
+if __name__ == "__main__":
+    main()
